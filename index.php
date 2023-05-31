96a9af3ed6925adeab11ead921babaa8ade8ecd1
Brayan Esteves <brayan.esteves93@gmail.com>
2023-05-31 11:28:49 -0400

    Create class 'App' [31.05.2023]

--- index.php
+++ index.php
@@ -2,9 +2,13 @@
     ini_set('display_errors', 1);
     include_once './core/Request.php';
     include_once './core/Route.php';
+    include_once './core/App.php';
+    include_once './app/controller/MainController.php';
     include_once './routes/web.php';
     use Core\Route;
     $request = new Request();
+    App::assets($request->getPublicURL());
+    // echo App::getPath();
     $routes  = Route::getRoutes();
     $url     = $request->getURL();
     $request->validate($routes, $url);
